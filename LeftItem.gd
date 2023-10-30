cee93c5d917263241a6bbeb1a219cd81eb31ddce
sungshen <143220321+sungshen@users.noreply.github.com>
2023-10-31 08:42:40 +0900

    Update LeftItem.gd

--- LeftItem.gd
+++ LeftItem.gd
@@ -1,11 +1,14 @@
 extends Sprite2D
 
+var player = player.player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = Player.new()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	player = player.player
+	name = player.aitem[loop(player.selectitem-1)]
+	load(player.icon)
