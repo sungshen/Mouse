e1dd547bf73e3df3072aa659ee822e88b3545521
sungshen <143220321+sungshen@users.noreply.github.com>
2023-10-30 11:33:46 +0900

    Update enemyhitbox.gd

--- enemyhitbox.gd
+++ enemyhitbox.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 var attackdamage = 0
+var enemy
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	enemy = Enemy.new()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	enemy = enemy.enemy
 	position = Vector2(50,50)
+        attackdamage = enemy.attackdamage
