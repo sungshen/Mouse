065bfe1cf8db700ba9892bb84dd2e097c04ce498
sungshen <143220321+sungshen@users.noreply.github.com>
2023-10-31 14:30:09 +0900

    Update SelectedItem.gd

--- SelectedItem.gd
+++ SelectedItem.gd
@@ -1,11 +1,21 @@
 extends Sprite2D
 
+var player = player.player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = Player.new()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	player = player.player
+	load(player.icons + '/' + player.AItems[loop(player.SelectItem,player.len(AItems))] + '.png')
+
+func loop(a,b):
+	if(a == -1):
+		return b
+	elif(a == b+1):
+		return 0
+	else:
+		return a
