ae8e0ef9a8c5cb03b2915da14ac3d327b120c5f3
sungshen <143220321+sungshen@users.noreply.github.com>
2023-10-31 19:29:41 +0900

    Update enemy.gd

--- enemy.gd
+++ enemy.gd
@@ -25,6 +25,7 @@ func on_heartbox_area_entered(area):
 		HP -= player.attackdamage
 		await get_tree().create_timer(0.1).timeout
 	if(player.attackey == 30 && Countable == true):
+		HP *= 9/10
 		delay = true
 		await get_tree().create_timer(15).timeout
 		delay = false
