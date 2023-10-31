6451deb532969b3060a2c12f9a4ccd221788fcdb
sungshen <143220321+sungshen@users.noreply.github.com>
2023-10-31 14:33:04 +0900

    Update TargetHPBar.gd

--- TargetHPBar.gd
+++ TargetHPBar.gd
@@ -1,11 +1,13 @@
-extends ProgressBar
+extends Sprite2D
 
+var player = player.player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = Player.new()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	player = player.player
+	
